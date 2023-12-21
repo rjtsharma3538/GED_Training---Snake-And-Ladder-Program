da2bfc96ad4fd9772e67f7f1c62c8ca13c45e341
Rajat Sharma <85641102+rjtsharma3538@users.noreply.github.com>
2023-12-21 14:05:36 +0530

    first-commit

--- src/App.java
+++ src/App.java
@@ -2,7 +2,45 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to Snake and ladder problem");
 
-        int dice_num = (int) Math.floor((Math.random() * 6) + 1);
-        System.out.println("Dice rolled " + dice_num);
+        int start_pos = 0;
+        int curr_pos = start_pos;
+
+        int no_move = 0;
+        int ladder = 1;
+        int snake = 2;
+
+        int chance = 0;
+
+        while (curr_pos < 100) {
+            int dice_num = (int) Math.floor((Math.random() * 6) + 1);
+            System.out.println("Dice rolled " + dice_num);
+
+            int option = (int) Math.floor((Math.random() * 3));
+
+            if (option == no_move) {
+                System.out.println("no_move -> " + curr_pos);
+            } else if (option == ladder) {
+                if (curr_pos + dice_num == 100)
+                {
+                    curr_pos=100;
+                    System.out.println(" u won -> " + curr_pos);
+                }
+                else if (curr_pos + dice_num < 100)
+                {
+                    curr_pos=curr_pos+dice_num;
+                    System.out.println("ladder -> " + curr_pos);
+                }
+                else
+                    System.out.println("ladder but no move -> " + curr_pos);
+            } else {
+                curr_pos = Math.max(0, curr_pos - dice_num);
+                System.out.println("snake -> " + curr_pos);
+            }
+            chance++;
+        }   
+
+        System.out.println("The chances taken by player to win are " + chance);
     }
+
+
 }
