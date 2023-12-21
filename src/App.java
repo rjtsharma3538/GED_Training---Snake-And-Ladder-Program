e9b714bdbdc3e01a3221def5301feddb7fc8768f
Rajat Sharma <85641102+rjtsharma3538@users.noreply.github.com>
2023-12-21 13:22:49 +0530

    first-commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to Snake and ladder problem");
+
+        int dice_num = (int) Math.floor((Math.random() * 6) + 1);
+        System.out.println("Dice rolled " + dice_num);
     }
 }
